7c3571f6fdf98932e2b34b472926b5394aa7e807
Sergio Garcia-Vergara <sergiodotgarcia@gmail.com>
2023-12-25 09:07:47 -0500

    Removed debugging statements

--- assets/js/toc-script.js
+++ assets/js/toc-script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('==============================================');
     var tocContainer = document.getElementById('table-of-contents');
     var headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
