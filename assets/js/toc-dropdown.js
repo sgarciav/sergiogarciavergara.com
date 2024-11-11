8e43341f1341423e9cbd1e3f8008c1c500bb4222
Sergio Garcia <sergiodotgarcia@gmail.com>
2024-11-11 12:50:36 -0500

    TOC lists do not add the webpage title and Contact me items

--- assets/js/toc-dropdown.js
+++ assets/js/toc-dropdown.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tocMenu = document.getElementById("toc-menu");
   const headers = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+  var count = 0;
 
   headers.forEach(header => {
+    // const headerText = header.textContent.trim();
+    // if (headerText === "Contact me") return;
+    count = count + 1; // Do not add the "Contact me" tag and Webpage title
+    if (count < 3) { return; }
     const level = parseInt(header.tagName.charAt(1)) - 1;
     const id = header.textContent.replace(/\s+/g, "-").toLowerCase();
     header.id = id;
